1cae188c075cd9c02b25b4ba4d45c0f930f17e8a
artssoria <53129227+artssoria@users.noreply.github.com>
2023-01-17 20:03:29 -0300

    subida de proyecto

---
+++ src/app/components/tareas/tareas.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Tarea } from 'src/app/models/Tarea';
+
+@Component({
+  selector: 'app-tareas',
+  templateUrl: './tareas.component.html',
+  styleUrls: ['./tareas.component.css']
+})
+export class TareasComponent {
+  listTareas: Tarea[] = []
+  nombreTarea = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  agregarTarea() {
+    // crear un  objeto tarea
+    const tarea: Tarea = {
+      nombre: this.nombreTarea,
+      estado: false
+    }
+    //agregar objeto tarea al array 
+    this.listTareas.push(tarea);
+    //resetear input
+    this.nombreTarea = '';
+  }
+
+  eliminarTarea(index: number): void {
+    this.listTareas.splice(index, 1);
+  }
+
+  actualizarTarea(index: number, tarea: Tarea): void{
+this.listTareas[index].estado = !tarea.estado;
+  }
+}
